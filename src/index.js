2cf39a8f8969b76f0fc1ffebacd7ffb980fe1ee2
白双椿 <740962631@qq.com>
2019-12-12 17:14:31 +0800

    删除原文件,修改新入口文件
    引进mobx , antd ,  Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Router } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+import './assets/styles/common.scss';
+import { ConfigProvider } from 'antd';
+import routes from './router';
+import store from './store';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import utils from './utils';
+moment.locale('zh-cn');
+// utils.history.listen((location:any, action:any) => {
+//   console.log(location, action);
+// })
+ReactDOM.render(
+  <ConfigProvider locale={zh_CN}>
+    <Provider {...store}>
+      <Router history={utils.history}>{routes}</Router>
+    </Provider>
+  </ConfigProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
